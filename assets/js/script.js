1d859d76a859edf1cb39e564cd43dd60860f83ba
Lucas Bezerra <lucas10besilva@gmail.com>
2024-03-27 21:56:39 -0300

    Finalização do Rodapé
    Finalizei o rodapé, inclui uma function no  botão de copia de e-mail, onde o e-mail do proprietário do site é copiado para a area de transferencia do usuário e finalizei a responsividade do rodapé.
    
    Inseri uma comunicação com o numero do proprietário, assim que o usuário clicar no botão de whatsapp.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,42 @@ function menuShow() {
     }
 }
 
+function copyButton() {
+    document.getElementById("copyButton").addEventListener("click", function() {
+        // Seleciona o email
+        var email = "lucas10besilva@gmail.com";
+        
+        // Cria um elemento de input temporário
+        var input = document.createElement("input");
+        input.setAttribute("value", email);
+        document.body.appendChild(input);
+        
+        // Seleciona o texto do input
+        input.select();
+        
+        // Copia o texto para a área de transferência
+        document.execCommand("copy");
+        
+        // Remove o input temporário
+        document.body.removeChild(input);
+        
+        // Alerta o usuário que o email foi copiado
+        alert("O email foi copiado para a área de transferência: " + email);
+      });
+}
+
+function whatsappButton() {
+    document.getElementById("whatsappButton").addEventListener("click", function() {
+        // Número de telefone para iniciar a conversa no WhatsApp (incluindo o código do país)
+        var telefone = "5513988313339"; // Substitua por seu número de telefone
+        
+        // Mensagem inicial opcional
+        var mensagem = "Olá, tudo bem?";
+        
+        // Cria o link para iniciar a conversa no WhatsApp
+        var link = "https://wa.me/" + telefone + "?text=" + encodeURIComponent(mensagem);
+        
+        // Redireciona o usuário para o link do WhatsApp
+        window.open(link, '_blank');
+      });
+}
